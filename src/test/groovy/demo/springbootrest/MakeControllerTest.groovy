dc4e4df7380b6fe6a1c6b35c661d2b5c206f89b3
ashaleyko <artem@shaleyko.org.ua>
2019-09-28 11:51:29 +0200

    fix get make by name test

--- src/test/groovy/demo/springbootrest/MakeControllerTest.groovy
+++ src/test/groovy/demo/springbootrest/MakeControllerTest.groovy
@@ -1,12 +1,9 @@
 package demo.springbootrest
 
-
-import demo.springbootrest.make.MakeRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.MvcResult
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import spock.lang.Specification
@@ -16,20 +13,17 @@ import spock.lang.Specification
 class MakeControllerTest extends Specification {
 
     @Autowired
-    private MakeRepository makeRepository = Mock()
-
-    @Autowired
     private MockMvc mvc
 
     def "should return 200 status for /makes"() {
         expect: "status is 200"
-        mvc.perform(MockMvcRequestBuilders.get("/makes"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
+            mvc.perform(MockMvcRequestBuilders.get("/makes"))
+                    .andExpect(MockMvcResultMatchers.status().isOk())
     }
 
     def "should return 200 status when get by name request is sent"() {
         expect: "status is 200 when get by name request is sent"
-            MvcResult result = mvc.perform(MockMvcRequestBuilders.get("/makes/name/Honda"))
-                .andExpect(MockMvcResultMatchers.status().isOk()).andReturn()
+            mvc.perform(MockMvcRequestBuilders.get("/makes/name/Honda"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
     }
 }
